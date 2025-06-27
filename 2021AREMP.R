87fd2c628d6499d6f88afc5d06ced8f355d72282
SavannahMilburnUGA <Savannah.Milburn@uga.edu>
2025-06-27 09:28:54 -0400

    2021AREMP - Commit 02: Conclusions on overlapping dates

--- 2021AREMP.R
+++ 2021AREMP.R
@@ -6,6 +6,8 @@ rm(list = ls())
 # Close old plots
 while (!is.null(dev.list())) dev.off()
 
+# WANT: data frame of deploy - date - Year - time - tempC
+# Note: 4 deploys only
 # 	1. Creating site table using the Filename from master spreadsheet
 		# a. Columns = lat, long, siteID
 # Eventually other site attributes/landscape variables, and daily mean/max from 2021 (only commonly sampled dates)
@@ -55,11 +57,18 @@ analyzeDeploy <- function(deployDF, deployID) {
     # Return summary row
     return(data.frame(deploy_ID=deployID, startDate=startDate, endDate=endDate, totalDays=totalDays, missingDays=missingDays))
 }
-# Apply analyzeDeploy function to 4 deploy sites
+# Apply analyzeDeploy function to 4 deploy sites to find overlapping dates
 AREMP2021Summary <- rbind(AREMP2021Summary, analyzeDeploy(deploy_ORCUB001IN02, "ORCUB001IN02"))
 AREMP2021Summary <- rbind(AREMP2021Summary, analyzeDeploy(deploy_ORHOT001IN01, "ORHOT001IN01"))
 AREMP2021Summary <- rbind(AREMP2021Summary, analyzeDeploy(deploy_ORHRK001IN01, "ORHRK001IN01"))
 AREMP2021Summary <- rbind(AREMP2021Summary, analyzeDeploy(deploy_ORHRK001IN02, "ORHRK001IN02"))
 AREMP2021Summary
-# Need to manually check AREMP2021Summary based on AREMP file
-# Looks like 4 AREMP sites could only do: 9/17/2021 to 5/25/2021 ?
+# Saving AREMP2021Summary as CSV file
+write.csv(AREMP2021Summary, "AREMP2021Summary.csv", row.names=FALSE)
+## Conclusions: doesn't seem very able to mesh w/ CRB + 4 USGS sites from 2021...
+# Only 1/4 deploys could be added to CRB + 4 USGS sites
+# For deploy sites ONLY could do: 9/17/2021 to 5/25/2021 OR 9/17/2021 to 12/31/2021 EXCEPT 1: deploy_ORHOT001IN01
+# deploy_ORCUB001IN02 : 1/1/2021 - 12/31/2021 // has FULL YEAR
+# deploy_ORHOT001IN01 : 1/1/2021 - 5/25/2021 // won't make it thru
+# deploy_ORHRK001IN01 : 9/17/2021 - 12/31/2021 // won't make it thru
+# deploy_ORHRK001IN02 : 9/17/2021 - 12/31/2021 // won't make it thru
